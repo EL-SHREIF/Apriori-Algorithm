d3f83007f2cb20dccb163c9ffd792097bdf09aef
aliamaim <ali.amaim@yahoo.com>
2020-05-06 15:46:41 -0700

    Update translate_row_names.r

--- functions/translate_row_names.r
+++ functions/translate_row_names.r
@@ -1,13 +1,12 @@
 translate_row_names <- function(mydata) {
   current_names = c("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K")
-  org_names = c('houses_', 'avg_size_household_', 'avg_age_', 'customer_main_type_',
-                'roman_catholic_', 'protestant_', 'other_religion_', 'no_religion_',
-                'married_', 'living_together_', 'other_relation_', 'singles_')
+  org_names = c('houses=', 'avg_size_household=', 'avg_age=', 'customer_main_type=',
+                'roman_catholic=', 'protestant=', 'other_religion=', 'no_religion=',
+                'married=', 'living_together=', 'other_relation=', 'singles=')
   
-  for (row in 1:nrow(mydata)){
-    for (i in 1:length(current_names)){
-      rownames(mydata)[row] = str_replace_all(rownames(mydata)[row], current_names[i], org_names[i])
-    }
+  for (i in 1:length(current_names)){
+     rownames(mydata) = str_replace_all(rownames(mydata), current_names[i], org_names[i])
   }
+
   return(mydata)
 }
