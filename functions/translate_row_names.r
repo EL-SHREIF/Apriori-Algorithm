937c24bc3a2a2ed2aa49b7289e9351716acdb4bc
aliamaim <ali.amaim@yahoo.com>
2020-05-06 13:20:19 -0700

    Update translate_row_names.r

--- functions/translate_row_names.r
+++ functions/translate_row_names.r
@@ -1,18 +1,13 @@
 translate_row_names <- function(mydata) {
+  current_names = c("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K")
+  org_names = c('houses_', 'avg_size_household_', 'avg_age_', 'customer_main_type_',
+                'roman_catholic_', 'protestant_', 'other_religion_', 'no_religion_',
+                'married_', 'living_together_', 'other_relation_', 'singles_')
   
   for (row in 1:nrow(mydata)){
-    rownames(mydata)[row] = str_replace_all(rownames(mydata)[row], 'A', 'houses_')
-    rownames(mydata)[row] = str_replace_all(rownames(mydata)[row], 'B', 'avg_size_household_')
-    rownames(mydata)[row] = str_replace_all(rownames(mydata)[row], 'C', 'avg_age_')
-    rownames(mydata)[row] = str_replace_all(rownames(mydata)[row], 'D', 'customer_main_type_')
-    rownames(mydata)[row] = str_replace_all(rownames(mydata)[row], 'E', 'roman_catholic_')
-    rownames(mydata)[row] = str_replace_all(rownames(mydata)[row], 'F', 'protestant_')
-    rownames(mydata)[row] = str_replace_all(rownames(mydata)[row], 'G', 'other_religion_')
-    rownames(mydata)[row] = str_replace_all(rownames(mydata)[row], 'H', 'no_religion_')
-    rownames(mydata)[row] = str_replace_all(rownames(mydata)[row], 'I', 'married_')
-    rownames(mydata)[row] = str_replace_all(rownames(mydata)[row], 'J', 'living_together_')
-    rownames(mydata)[row] = str_replace_all(rownames(mydata)[row], 'K', 'other_relation_')
-    rownames(mydata)[row] = str_replace_all(rownames(mydata)[row], 'L', 'singles_')
+    for (i in 1:length(current_names)){
+      rownames(mydata)[row] = str_replace_all(rownames(mydata)[row], current_names[i], org_names[i])
+    }
   }
   return(mydata)
 }
